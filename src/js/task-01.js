f43494a8747301e197ed274b6526f6f556dbb178
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-01-23 00:13:09 +0200

    add task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,12 @@
-const listEl = document.querySelector('#categories');
-console.log(listEl);
+const categoriesListEl = document.querySelector('#categories');
+console.log(categoriesListEl);
+
+const itemsList = categoriesListEl.querySelectorAll(".item");
+console.log(itemsList);
+
+console.log('Number of categories:', itemsList.length);
+
+itemsList.forEach(item => {
+    const itemTitle = item.querySelector('h2')
+    console.log(itemTitle)
+})
